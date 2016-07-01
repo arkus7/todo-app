e000286d588a35e682a778ff3ff94476feef70a5
Arkadiusz Żmudzin <arkus7@users.noreply.github.com>
2016-07-01 20:11:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // mongo database
 var uristring =
-  process.env.MONGOLAB_URI ||
+  process.env.MONGODB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://todo-app-dbuser:todoapp@ds011432.mlab.com:11432/heroku_b1s4pcsr';
 
